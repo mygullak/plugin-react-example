2e48c9b3a468462a502f11b93d8dd60c69c05953
junaid <junaid1460@gmail.com>
2022-09-20 03:08:14 +0530

    feat: init plugin example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
+
+
+
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const script = document.createElement('script');
+      script.src = 'https://plugin.myhubble.money/hubble-1.0.1.js'; 
+      script.onerror = () => {
+        
+      };
+      script.onload = async () => {
+        window.hubble.init({clientID: 'croma'});
+        setLoading(false);
+      };
+      document.body.appendChild(script);
+  })
+
+  // Here on click of button we are calling the openHubble function
+  // You can call this function from anywhere in your app
+  // For example you can call this function from a button click
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    isLoading ? 'Loading..':
+      <button onClick={() => window.hubble.open()}>start</button>
   );
 }
 
